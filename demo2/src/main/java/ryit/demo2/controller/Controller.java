1788545f17074e5a2893637f18e0d34522de0d52
lvxiang <1035730889@qq.com>
2022-05-19 18:02:11 +0800

    3

--- demo2/src/main/java/ryit/demo2/controller/Controller.java
+++ demo2/src/main/java/ryit/demo2/controller/Controller.java
@@ -1,12 +1,14 @@
 package ryit.demo2.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class Controller {
     @RequestMapping("/get")
     public void get(){
-
+      log.info("get开始了");
     }
 }
