706e9c2f05e3d6f243aa4658b88ab19253e51808
lvxiang <1035730889@qq.com>
2022-05-19 18:01:27 +0800

    2

--- demo2/src/main/java/ryit/demo2/controller/Controller.java
+++ demo2/src/main/java/ryit/demo2/controller/Controller.java
@@ -1,4 +1,12 @@
 package ryit.demo2.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+    @RequestMapping("/get")
+    public void get(){
+
+    }
 }
